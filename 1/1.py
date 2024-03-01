9b5521e34ff236fea3618a44dd6d74609716ce2d
mariasofia200 <mariarincon1@colegioguanenta.edu.co>
2024-03-01 08:14:45 -0500

    se realizo analisi y diseño

---
+++ 1/1.py
@@ -0,0 +1,23 @@
+#programa para calcular en que cuadrante esta un punto, de un plano cartesiano 
+
+#entrada
+X = int(input("ingrese la coordenada x: "))
+Y = int(input("ingrese la coordenada y: "))
+
+# proceso y salida
+if X == 0:
+    if Y == 0:
+        print("la coordenada" ,(X , Y),"este en el origen")
+    else:
+        print("la coordenada" , (X , Y),"esta en el eje Y")
+elif Y == 0:
+    print("la coordenada" ,(X , Y),"esta en el eje x")
+elif X > 0:
+    if Y > 0:
+        print("la coordenada" ,(X , Y)," esta en el cuadrante 1")
+    else:
+        print("la coordenada" ,(X , Y),"esta en el cuadrante 4")
+elif Y < 0:
+    print(" la coordenada" ,(X , Y),"esta en el cuadrante 3")
+else:
+    print("la coordenada" ,(X , Y),"esta ej el cuadrante 2")
---
+++ 1/README.md
@@ -0,0 +1,26 @@
+# instruccionescondicionales
+
+# un programa lector de cordenadas  cartecianas (x,y) de un punto en el plano y calcule el cuadrante el cual le pertenece un punto
+
+# ANALISIS
+
+varible de entrada y proceso (input, processing)
+
+x,y: coordenadas del punto
+
+variable de salida 
+
+Eje x
+Eje y
+Origen 
+Cuadrante 1
+Cuadrante 2
+Cuafrante 3
+Cuafrante 4
+
+Nos idica el punto exacto en el plano cartesiano
+
+# DISEÑO
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
+
+# CONSTRUCCION
